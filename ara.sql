5398d038edd4c3f96ba682e5497ca123f6a899e7
adamadamp <162697883+adamadamp@users.noreply.github.com>
2024-06-13 18:51:08 +0200

    Add files via upload

---
+++ ara.sql
@@ -0,0 +1,49 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le : jeu. 13 juin 2024 à 16:47
+-- Version du serveur : 8.0.36
+-- Version de PHP : 8.2.13
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `fasb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `ara`
+--
+
+DROP TABLE IF EXISTS `ara`;
+CREATE TABLE IF NOT EXISTS `ara` (
+  `a` varchar(50) DEFAULT NULL,
+  `b` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Déchargement des données de la table `ara`
+--
+
+INSERT INTO `ara` (`a`, `b`) VALUES
+('2222', 'bbhgf'),
+('aaaa', 'QQQQ'),
+('root', '12345'),
+('0q343', 'ADAM');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
